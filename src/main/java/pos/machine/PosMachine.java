f9a808d9c1484f635d7d1bfea9424299a7b61180
Joyce Ng <joyce.ng@oocl.com>
2025-09-03 20:52:33 +0800

    feat: add item and receipt classes

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -3,6 +3,50 @@ package pos.machine;
 import java.util.List;
 
 public class PosMachine {
+        static class ItemQuantity {
+        String barcode;
+        String name;
+        int quantity;
+        ItemQuantity(String barcode, String name, int quantity) {
+            this.barcode = barcode;
+            this.name = name;
+            this.quantity = quantity;
+        }
+    }
+    static class ItemWithPrice {
+        String barcode;
+        String name;
+        int quantity;
+        int unitPrice;
+        ItemWithPrice(String barcode, String name, int quantity, int unitPrice) {
+            this.barcode = barcode;
+            this.name = name;
+            this.quantity = quantity;
+            this.unitPrice = unitPrice;
+        }
+    }
+    static class ReceiptItem {
+        String barcode;
+        String name;
+        int quantity;
+        int unitPrice;
+        int subTotal;
+        ReceiptItem(String barcode, String name, int quantity, int unitPrice, int subTotal) {
+            this.barcode = barcode;
+            this.name = name;
+            this.quantity = quantity;
+            this.unitPrice = unitPrice;
+            this.subTotal = subTotal;
+        }
+    }
+    private static class AmountResult {
+    List<ReceiptItem> receiptItems;
+    int total;
+    AmountResult(List<ReceiptItem> receiptItems, int total) {
+        this.receiptItems = receiptItems;
+        this.total = total;
+    }
+    }
     public String printReceipt(List<String> barcodes) {
         return null;
     }
